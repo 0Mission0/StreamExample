e7b9c676ddfa822abe9f7311d03f6e6ba00dbb0e
0Mission0 <0mission0@gmail.com>
2018-08-28 11:21:50 +0800

    加上 Join 跟 Distinct 兩種範例
    小小修改一下寫法

--- src/idv/mission/example/stream/DistinctExample.java
+++ src/idv/mission/example/stream/DistinctExample.java
@@ -1,20 +1,18 @@
 package idv.mission.example.stream;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class DistinctExample {
 	public static void main(String[] args) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("1", "1");
-		map.put("2", "2");
-		map.put("3", "2");
-		map.put("4", "2");
-		map.put("5", "3");
-		
-        List<String> result = map.values().stream().distinct().collect(Collectors.toList());
+		List<String> result = new ArrayList<String>();
+		result.add("1");
+		result.add("2");
+		result.add("2");
+		result.add("2");
+		result.add("3");
+        result = result.stream().distinct().collect(Collectors.toList());
         System.out.println(result);
 	}
 }
